43fae15065c096a0e6d6114dcfbf7c71b34dc3eb
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-03-15 09:53:17 -0700

    fix: replace jest stub with filetransformer to clean up warnings in tests

---
+++ fileTransformer.js
@@ -0,0 +1,10 @@
+//const path = require('path');
+import path from 'path'
+
+export default{
+  process(sourceText, sourcePath, options) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ export default {
   testEnvironment: 'jsdom',
   transform: {
     "^.+\\.tsx?$": ["ts-jest", {"tsconfig": "./tsconfig.app.json"}],
-    ".+\\.(css|less|sass|scss|png|jpg|gif|ttf|woff|woff2|svg)$":
-    "jest-transform-stub",
+    ".+\\.(css|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    '<rootDir>/fileTransformer.js',
   }
 }
